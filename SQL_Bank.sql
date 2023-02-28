2b91435061e1327b705d020571c7fc6c8a5a2a8e
Ben from Ironhack <benlned@gmail.com>
2023-02-28 17:36:49 +0100

    Solution SQL_Bank

---
+++ SQL_Bank.sql
@@ -0,0 +1,15 @@
+SELECT * FROM actor;
+
+SELECT * FROM film;
+
+SELECT * FROM customer;
+
+SELECT title FROM film;
+
+SELECT distinct name FROM language;
+
+SELECT distinct address_id FROM store;
+
+SELECT count(staff_id) FROM staff;
+
+SELECT first_name FROM staff;
